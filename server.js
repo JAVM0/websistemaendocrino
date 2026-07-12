4963e36a8d384859368fe0e1ed0b3e3da72eae95
JAV <javicho201410@gmail.com>
2026-07-12 04:47:27 -0500

    Update server.js

--- server.js
+++ server.js
@@ -4,24 +4,21 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname), {
-  setHeaders(res, filePath) {
-    if (filePath.endsWith('.mp4')) {
-      res.setHeader('Accept-Ranges', 'bytes');
-    }
-  }
-}));
+function serveStatic(folder) {
+  return (req, res) => {
+    const filePath = path.join(__dirname, folder, req.params[0]);
+    res.sendFile(filePath, (err) => {
+      if (err) res.status(404).end();
+    });
+  };
+}
+
+app.get('/css/*', serveStatic('css'));
+app.get('/js/*', serveStatic('js'));
+app.get('/assets/*', serveStatic('assets'));
 
 app.get('*', (_req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
-});
-
-app.get('*/css', (_req, res) => {
-  res.sendFile(path.join(__dirname, 'styles.css'));
-});
-
-app.get('*/js', (_req, res) => {
-  res.sendFile(path.join(__dirname, 'main.js'));
 });
 
 app.listen(PORT, () => {
