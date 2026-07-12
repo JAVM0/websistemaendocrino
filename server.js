6c6cbcf9ab1bf9627cda1cac0bd2fe5c1b4858e7
JAV <javicho201410@gmail.com>
2026-07-12 04:52:55 -0500

    b

--- server.js
+++ server.js
@@ -4,19 +4,6 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-function serveStatic(folder) {
-  return (req, res) => {
-    const filePath = path.join(__dirname, folder, req.params[0]);
-    res.sendFile(filePath, (err) => {
-      if (err) res.status(404).end();
-    });
-  };
-}
-
-app.get('/css/*', serveStatic('css'));
-app.get('/js/*', serveStatic('js'));
-app.get('/assets/*', serveStatic('assets'));
-
 app.get('*', (_req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
